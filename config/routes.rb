5fc6e4a92ebdc17bd1881b4396a56aedce11a92f
Jason Force <jason.r.force@gmail.com>
2022-08-19 22:56:46 -0500

    Add User model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :locations
   resources :rsvps
+  resources :users
   root 'welcome#index'
+
+  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
